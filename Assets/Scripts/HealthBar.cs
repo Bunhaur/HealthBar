79b4e90bc3811f47ec806cc703ccc6e16195ffb9
Bunhaur <bunhau@yandex.ru>
2023-05-21 10:26:43 +0400

    rescripting

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -1,3 +1,33 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class HealthBar : MonoBehaviour{}
+[RequireComponent(typeof(Slider))]
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] Player _player;
+    [SerializeField] private Text _currentHealthText;
+
+    private Slider _slider;
+    private float _currentHealth;
+
+    private void Start() => _slider = GetComponent<Slider>();
+
+    private void Update()
+    {
+        _currentHealth = _player.CurrentHealth;
+        _currentHealthText.text = $"{_currentHealth}/{_slider.maxValue}";
+    }
+
+    public void ChangeHealth(float value) => StartCoroutine(ChangeHealthBar(value));
+
+    private IEnumerator ChangeHealthBar(float value)
+    {
+        while (_slider.value != value)
+        {
+            _slider.value = Mathf.MoveTowards(_slider.value, _currentHealth, 25F * Time.deltaTime);
+            yield return null;
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,66 +1,37 @@
-using System.Collections;
 using UnityEngine;
-using UnityEngine.UI;
-
-[RequireComponent(typeof(Slider))]
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private Text _currentHealthText;
-
-    private Slider _health;
+    [SerializeField] private HealthBar _healthBar;
 
     private float _maxHealth;
     private float _minHealth;
     private float _damage;
     private float _heal;
     private float _currentHealth;
-    private float _changeSpeed;
+
+    public float CurrentHealth => _currentHealth;
 
     private void Start()
     {
-        _health = GetComponent<Slider>();
-
         _maxHealth = 100f;
         _minHealth = 0.0f;
-        _health.value = _maxHealth;
         _damage = 10f;
         _heal = 10f;
-        _currentHealth = _health.value;
-        _changeSpeed = 25f;
-    }
-
-    private void Update()
-    {
-        _currentHealthText.text = $"{_currentHealth}/{_maxHealth}";
+        _currentHealth = _maxHealth;
     }
 
     public void TakeDamage()
     {
-        if (_currentHealth - _damage < _minHealth)
-            _currentHealth = _minHealth;
-        else 
-            _currentHealth -= _damage;
+        _currentHealth = Mathf.Clamp(_currentHealth - _damage, _minHealth, _maxHealth);
 
-        StartCoroutine(HealthChanged());
+        _healthBar.ChangeHealth(_currentHealth);
     }
 
     public void TakeHealth()
     {
-        if (_currentHealth + _heal > _maxHealth)
-            _currentHealth = _maxHealth;
-        else
-            _currentHealth += _heal;
+        _currentHealth = Mathf.Clamp(_currentHealth + _heal, _minHealth, _maxHealth);
 
-        StartCoroutine(HealthChanged());
-    }
-
-    private IEnumerator HealthChanged()
-    {
-        while (_health.value != _currentHealth)
-        {
-            _health.value = Mathf.MoveTowards(_health.value, _currentHealth, _changeSpeed * Time.deltaTime);
-            yield return null;
-        }
+        _healthBar.ChangeHealth(_currentHealth);
     }
 }
